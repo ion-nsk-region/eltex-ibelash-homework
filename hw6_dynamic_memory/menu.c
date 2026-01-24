fcecf1624f776266458ee3e5998e60c0efd8e57d
Ivan Belash <passdamicrophone@gmail.com>
2026-01-24 10:19:03 +0700

    Обработать некорректный ввод меню.

--- hw6_dynamic_memory/menu.c
+++ hw6_dynamic_memory/menu.c
@@ -1,17 +1,20 @@
 #include "abonent_dir.h"
 
 int menu() {
-  int item;
+  int item, ret, c;
   printf("\n1) Добавить абонента\n");
   printf("2) Удалить абонента\n");
   printf("3) Поиск абонентов по имени\n");
   printf("4) Вывод всех записей\n");
   printf("5) Выход\n");
 
-  if (1 != scanf("%d", &item)) {
+  ret = scanf("%d", &item);
+  // очищаем буфер stdin чтобы не было срабатываний на некорректный ввод
+  while('\n' != (c = getchar()) && EOF != c);  
+
+  if (1 != ret) {
     return -1;
   }
-  scanf("%*c");  // считываем и выбрасываем перенос строки из буфера
 
   if (item < 1 || item > 5) {
     return -1;
