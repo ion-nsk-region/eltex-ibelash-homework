679b5e3d4e911b3ccd5abb2d5a9e58ca9d05bdcc
Ivan Belash <passdamicrophone@gmail.com>
2026-01-27 00:48:00 +0700

    Домашняя работа 6: Динамическая память - Баг Очистки справочника
    Справочник не до конца очищался если число абонентов было три и больше.

--- hw6_dynamic_memory/clear_dir.c
+++ hw6_dynamic_memory/clear_dir.c
@@ -1,7 +1,15 @@
 #include "abonent_dir.h"
 
 void clear_dir(struct Directory *dir) {
-  for (int i = 0; i <= dir->size; i++) {
-    abonent_del(dir, dir->tail);
+  int res, dir_size = dir->size;
+
+  for (int i = 0; i < dir_size; i++) {
+    res = abonent_del(dir, dir->tail);
+    if (0 != res) {
+      printf(
+          "Произошла ошибка во время удаления."
+          "Код ошибки: %d\n",
+          res);
+    }
   }
 }
