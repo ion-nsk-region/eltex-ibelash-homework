d24ef3a805a0759abcf41995410bf313088b09d4
Ivan Belash <passdamicrophone@gmail.com>
2026-02-25 00:38:52 +0700

    Домашнее задание 9: Файловая система - Файловый менеджер: Выход по q

--- hw9_filesystem/ex_2/eltex_commander.c
+++ hw9_filesystem/ex_2/eltex_commander.c
@@ -1,18 +1,25 @@
 #include "eltex_commander.h"
 
 int main() {
-    int ret = 0;
+    int ret = 0, line = 0, col = 0;
     ret = initialize_terminal();
     if (0 > ret) {
         printf("Не удалось инициализировать терминал.\n"
                         "Код ошибки: %d\nВыходим.\n", ret);
     }
     
-    printw("Привет! 🤓\n");
-    printw("Мои размеры %d x %d\n", LINES, COLS);
+    printw("Привет! 🤓\n\n");
 
-    refresh();
-    getch();
+    do {
+        getyx(stdscr, line, col);
+        line = line - 1;
+        col = 0;
+        move(line, col);
+        clrtoeol();
+        printw("Мои размеры %d x %d\n", LINES, COLS);
+        refresh();
+    } while('q' != getch());
+    
 
     endwin();
     return ret;
