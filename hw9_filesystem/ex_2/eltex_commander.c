c0137b56817935035842ee73590c98bbd80d767f
Ivan Belash <passdamicrophone@gmail.com>
2026-02-28 19:55:10 +0700

    Домашнее задание 9: ex_2: без LINES и COLS
        Согласно https://invisible-island.net/ncurses/ncurses-intro.html#caution
    использование глобальных переменных LINES и COLS не рекомендуется, так
    как может быть открыто несколько экранов с разными размерами. То есть
    наша программа может выводить информацию в несколько терминалов. Таким
    образом, глобальные переменные будут хранить размеры последнего экрана
    (терминала), который может не совпадать с размерами терминала где запущена
    наша программа.
        Вместо глобальных переменных используем макро getmaxyx для получения
    размеров окна stdscr.

--- hw9_filesystem/ex_2/eltex_commander.c
+++ hw9_filesystem/ex_2/eltex_commander.c
@@ -1,8 +1,12 @@
 #include "eltex_commander.h"
 
 int main() {
-    int ret = 0, lp_y, lp_x, rp_y, rp_x;
+    int ret = 0, 
+        t_y, t_x, //< Размеры основного окна (терминала)
+        lp_y, lp_x, //< Размеры левой панели
+        rp_y, rp_x; //< Размеры правой панели
     WINDOW *left_panel, *right_panel;
+
     ret = initialize_terminal();
     if (0 > ret) {
         fprintf(stderr, "Не удалось инициализировать терминал.\n"
@@ -10,23 +14,38 @@ int main() {
         return ret;
     }
     
-    left_panel = derwin(stdscr, LINES-2, (COLS/2)-1, 1, 1);
-    right_panel = derwin(stdscr, LINES-2, (COLS/2)-1, 1, (COLS+1)/2);
+    getmaxyx(stdscr, t_y, t_x);
+    left_panel = derwin(stdscr, t_y - 2, (t_x / 2), 1, 0);
+    if (NULL == left_panel) {
+        fprintf(stderr, "Не удалось создать левую панель.\n"
+                        "Выходим.\n");
+        return -1;
+    }
+
+    right_panel = derwin(stdscr, t_y - 2, (t_x / 2), 1, (t_x + 1) / 2);
+    if (NULL == right_panel) {
+        fprintf(stderr, "Не удалось создать правую панель.\n"
+                        "Выходим.\n");
+        return -1;
+    }
 
     do {
-        werase(left_panel);
-        wresize(left_panel, LINES-2, (COLS/2)-1);
-        werase(right_panel);
-        mvderwin(right_panel, 1, COLS/2);
-        wresize(right_panel, LINES-2, ((COLS+1)/2)-1);
-        box(stdscr, '|', '-');
-        box(left_panel, 'l', '-');
-        box(right_panel, 'r', '-');
+        clear();
+
+        // Обрабатываем изменение размера терминала
+        getmaxyx(stdscr, t_y, t_x);
+        wresize(left_panel, t_y - 2, (t_x / 2));
+        mvderwin(right_panel, 1, t_x / 2);
+        wresize(right_panel, t_y - 2, ((t_x + 1) / 2));
+        box(left_panel, ACS_VLINE, ACS_HLINE);
+        box(right_panel, ACS_VLINE, ACS_HLINE);
+
+        // выводим размеры терминала, левой и правой панели.
         getmaxyx(left_panel, lp_y, lp_x);
         getmaxyx(right_panel, rp_y, rp_x);
-        wprintw(left_panel, "Размеры терминала %d x %d\n", LINES, COLS);
-        wprintw(left_panel, "Размеры панели %d x %d\n", lp_y, lp_x);
-        wprintw(right_panel, "Размеры панели %d x %d\n", rp_y, rp_x);
+        mvprintw(0, 0, "%dx%d  L %dx%d  R %dx%d", t_y, t_x, lp_y, lp_x, rp_y, rp_x);
+
+        // Собственно вывод на экран
         refresh();
     } while('q' != getch());
     
