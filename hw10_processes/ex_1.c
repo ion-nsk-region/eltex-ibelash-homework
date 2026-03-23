6ce314de7dc0e65c65d255d0f91c65f97dcf58ca
Ivan Belash <passdamicrophone@gmail.com>
2026-03-23 18:11:53 +0700

    hw10 ex_1: done

---
+++ hw10_processes/ex_1.c
@@ -0,0 +1,27 @@
+#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(void) {
+    pid_t child_pid;
+    int status;
+
+    errno = 0;
+    child_pid = fork();
+    if(-1 == child_pid) {
+        perror("fork");
+    } else if (0 == child_pid) {
+        printf("CHILD: I am a child with PID: %d and PPID: %d\n", getpid(), getppid());
+    } else {
+        printf("PARENT: I am a parent with PID: %d and PPID: %d\n", getpid(), getppid());
+        errno = 0;
+        if (child_pid == wait(&status) && WIFEXITED(status)) {
+            printf("PARENT: Our child exited with status %d\n", WEXITSTATUS(status));
+        } else {
+            perror("wait");
+        }
+    }
+
+    return 0;
+}
