3f2f3dd0c81fd6345ab694d54e69453ac8946bd0
Ivan Belash <passdamicrophone@gmail.com>
2026-04-10 16:36:26 +0700

    hw12 Каналы: ex_1 - Добавлена обработка ошибок и финальное закрытие каналов

--- hw12_pipes/ex_1_parent_child/ex_1.c
+++ hw12_pipes/ex_1_parent_child/ex_1.c
@@ -21,29 +21,37 @@ int main(void) {
   pid_t child_pid = fork();
   if (-1 == child_pid) {
     perror("fork");
-    err -= 1;
+    err = -1;
   } else if (0 < child_pid) {
     // родитель
     if (0 == close_pipe_end(my_pipe[0]) &&  // закрываем на чтение
         (0 <= send_msg_to_pipe(my_pipe, MSG_TO_SEND))) {
-      err = wait4child(child_pid);
+      if (-1 == (err = wait4child(child_pid))) {
+        printf(
+            "Ошибка при проверке состояния дочернего процесса. "
+            "См. подробности в stderr.\n");
+      }
     } else {
       printf(
           "Ошибка: не удалось отправить сообщение. "
           "См. подробности в stderr.\n");
-      err -= 1;
+      err = -2;
     }
+    // больше ничего писать не будем, поэтому закрываем и на запись.
+    close_pipe_end(my_pipe[1]);
   } else if (0 == child_pid) {
     // дочерний процесс
-    if (0 == close_pipe_end(my_pipe[1])) {  // закрываем на запись
-      read_msg_from_pipe(my_pipe, msg);
+    if (0 == close_pipe_end(my_pipe[1]) &&  // закрываем на запись
+        0 < read_msg_from_pipe(my_pipe, msg)) {
       printf("%s\n", msg);
     } else {
       printf(
-          "Ошибка: не удалось закрыть канал на запись. "
-          "Сообщение не было прочитано. См. подробности в stderr.\n");
-      err -= 1;
+          "Ошибка: не удалось прочитать сообщение. "
+          "См. подробности в stderr.\n");
+      err = -3;
     }
+    // больше ничего читать не будем, поэтому закрываем окончательно
+    close_pipe_end(my_pipe[0]);
   }
 
   return err;
--- hw12_pipes/ex_1_parent_child/wait4child.c
+++ hw12_pipes/ex_1_parent_child/wait4child.c
@@ -2,9 +2,11 @@
 
 int wait4child(pid_t child_pid) {
   int status;
-  waitpid(child_pid, &status, 0);
-
-  if (WIFEXITED(status) && 0 != status) {
+  errno = 0;
+  if (-1 == waitpid(child_pid, &status, 0)) {
+    perror("waitpid");
+    status = -1;
+  } else if (WIFEXITED(status) && 0 != status) {
     fprintf(stderr,
             "Предупреждение: Дочерний процесс вернул ненулевой статус: %d.\n",
             WEXITSTATUS(status));
