5bd8d79f947ff963d5e50e57bd147d6e820cbc76
Ivan Belash <passdamicrophone@gmail.com>
2026-06-12 15:20:00 +0700

    hw13 ex_3 Готово

---
+++ hw15_signals/ex_3/signal_receiver.c
@@ -0,0 +1,27 @@
+#include "signal_receiver.h"
+
+int main(void) {
+  int err = 0, signal_number = 0;
+  sigset_t signal_mask;
+
+  if (-1 == sigemptyset(&signal_mask) ||
+      -1 == sigaddset(&signal_mask, SIGUSR1)) {
+    err = errno;
+    perror("sigemptyset or sigaddset");
+  } 
+  
+  if (0 == err && 0 > (err = sigprocmask(SIG_BLOCK, &signal_mask, NULL))) {
+    perror("sigprocmask");
+  }
+
+  printf("Наш pid %d\n", getpid());
+
+  if (0 == err) {
+    while (1) {
+      sigwait(&signal_mask, &signal_number);
+    printf("Получен сигнал %s\n", strsignal(signal_number));
+    }
+  }
+
+  return err;
+}
---
+++ hw15_signals/ex_3/signal_receiver.h
@@ -0,0 +1,11 @@
+#ifndef SIGNAL_RECEIVER_H
+#define SIGNAL_RECEIVER_H
+
+#include <errno.h>
+#include <signal.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#endif  // SIGNAL_RECEIVER_H
