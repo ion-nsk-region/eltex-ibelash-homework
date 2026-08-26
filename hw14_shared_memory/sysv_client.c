5cc38486469f89f742962ba5e6332fa354f3399d
Ivan Belash <passdamicrophone@gmail.com>
2026-08-26 19:06:54 +0700

    WIP hw14_1_sysv Клиент

---
+++ hw14_shared_memory/sysv_client.c
@@ -0,0 +1,49 @@
+#include <stdlib.h>
+
+#include "shm_chat.h"
+
+int main(void) {
+  int err = 0, shm_id, sem4_id;
+  void *shm_addr = NULL;
+
+  char *msg = NULL;
+  char *reply = "Hello!";
+
+  key_t key = ftok(SERVER_FILENAME, PROJ_ID);
+  errno = 0;
+  shm_id = shmget(key, 1, 0);
+  if (-1 == shm_id) {
+    perror("shmget");
+    err = -1;
+    goto exit;
+  }
+
+  shm_addr = attach_shm_segment(shm_id);
+  if ((void *)-1 == shm_addr) {
+    fprintf(stderr,
+            "Ошибка: не удалось подключить сегмент разделяемой памяти. "
+            "Проверьте, что сервер запущен.\n");
+    err = -2;
+    goto exit;
+  }
+
+  errno = 0;
+  sem4_id = semget(key, 1, 0);
+  if (-1 == sem4_id) {
+    perror("semget");
+    err = -3;
+    goto exit;
+  }
+
+  set_state(sem4_id, CL_WAITING);
+  receive_msg(shm_addr, &msg);
+  printf("%s\n", msg);
+  send_msg(shm_addr, reply);
+  set_state(sem4_id, CL_MSG_SENT);
+
+  free(msg);
+
+exit:
+
+  return err;
+}
